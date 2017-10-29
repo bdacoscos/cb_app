8867bde4641f441b375e1ea7564db8e46217aa29
Briana Dacoscos <briana.dacoscos@gmail.com>
2017-10-29 16:58:08 -0700

    Add password_digest column

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   root 'pages#show', page: 'home'
 
+  get 'users/:id', to: 'users#show'
+
   resources :users, only: [:create, :new, :edit, :show, :update, :destroy]
 end
