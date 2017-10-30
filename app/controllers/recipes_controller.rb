0edfe36e6d74d865a74a3f54e5319938284f30ca
Briana Dacoscos <briana.dacoscos@gmail.com>
2017-10-30 14:55:58 -0700

    Allow user to create and save new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,9 +9,23 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      flash[:notice] = "Recipe saved!"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :image, :description, :ingredients, :instructions)
   end
 
 end
