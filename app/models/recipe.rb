7166b71343a3dbb846f27d1de52927e028d18749
Briana Dacoscos <briana.dacoscos@gmail.com>
2017-11-01 09:42:00 -0700

    Allow images to be uploaded in new recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  has_attached_file :image, styles: { medium: "500x500!", thumb: "100x100>" }, presence: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 end
